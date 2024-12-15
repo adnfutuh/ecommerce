9d353792d09148d8ed2537c22555c942fc71d450
rizki <rizkiadnanfutuh@gmail.com>
2024-12-15 15:37:36 +0700

    feat: add color, enum, and text classes for application needs

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,15 @@
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymenyMethods {
+  paypal,
+  googlePlay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorPay,
+  paytm,
+}
