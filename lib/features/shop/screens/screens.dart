cba7ba1bf507930fe1c9e2d4199c583617264a43
rizki <rizkiadnanfutuh@gmail.com>
2024-12-17 07:09:44 +0700

    feat: Implement navigation menu with 4 main screens

---
+++ lib/features/shop/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'home/home_screen.dart';
+export 'store/store_screen.dart';
+export 'wishlist/wishlist_screen.dart';
+export 'profile/profile_screen.dart';
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+import 'features/shop/screens/screens.dart';
+import 'utils/utils.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final dark = MyHelperFunctions.isDarkMode(context);
+    return Obx(
+      () => Scaffold(
+        bottomNavigationBar: NavigationBar(
+            height: 80,
+            elevation: 0,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (value) =>
+                controller.selectedIndex.value = value,
+            backgroundColor: dark ? MyColors.black : MyColors.white,
+            indicatorColor: dark
+                ? MyColors.white.withOpacity(0.1)
+                : MyColors.black.withOpacity(0.1),
+            destinations: const [
+              NavigationDestination(
+                  icon: Icon(Iconsax.home_outline), label: "Home"),
+              NavigationDestination(
+                  icon: Icon(Iconsax.shop_outline), label: "Store"),
+              NavigationDestination(
+                  icon: Icon(Iconsax.heart_outline), label: "Wishlist"),
+              NavigationDestination(
+                  icon: Icon(Iconsax.user_outline), label: "Profile"),
+            ]),
+        body: controller.screens[controller.selectedIndex.value],
+      ),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
+    const HomeScreen(),
+    const StoreScreen(),
+    const WishlistScreen(),
+    const ProfileScreen(),
+  ];
+}
