44f8df36d111d84266babd772b837b3041cd4fdb
rizki <rizkiadnanfutuh@gmail.com>
2024-12-26 23:01:47 +0700

    feat: integrate Firebase into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:ecommerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
